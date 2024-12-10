b1a2a110cfb8f059616b1eb71da6f60c5df8249c
retoso <renato.souza1969@yahoo.com.br>
2024-12-10 16:01:24 -0300

    arquivos criados basico

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          teste de front
+        </p>
+        <a>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
